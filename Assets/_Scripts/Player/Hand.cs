8157872ec8be5f5d31fc76480d2ef7f0a60b3b65
garrowbr <garrowbr@msu.edu>
2026-02-21 16:36:42 -0600

    Animate Player

--- Assets/_Scripts/Player/Hand.cs
+++ Assets/_Scripts/Player/Hand.cs
@@ -1,66 +1,59 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-  [Header("Dice Prefabs")]
-  [SerializeField] GameObject _d4Prefab;
-  [SerializeField] GameObject _d6Prefab;
-  [SerializeField] GameObject _d8Prefab;
-  [SerializeField] GameObject _d12Prefab;
-  [SerializeField] GameObject _d20Prefab;
 	Animator _anim;
-  GameObject _dieToShoot;
+
+	[Header("Projectile Holder")]
+	[SerializeField] GameObject _projectileHolder;
 
 	void Start()
 	{
-    gameObject.SetActive(false);
 		_anim = GetComponent<Animator>();
 	}
 
-  void Update()
-  {
-    //if(_dieToShoot != null && Animator.GetCurrentAnimatorStateInfo(0).normalizedTime>1 && !Animator.IsInTransition(0) )
-    if (_dieToShoot != null)
-    {
-      _dieToShoot.GetComponent<DiceProjectile>().Shoot();
-      _dieToShoot = null;
-    }
-  }
-
-  public void Throw()
-  {
-    int diceSides = WaveAuthority.PlayerRef.GetAmmo();
-    if (diceSides == 0)
-    {
-      // If there was no dice in bag, do nothing
-      return;
-    }
-    Animate();
-
-    switch (diceSides)
-    {
-      case 4:
-        _dieToShoot = Instantiate(_d4Prefab, transform);
-        break;
-      case 6:
-        _dieToShoot = Instantiate(_d6Prefab, transform);
-        break;
-      case 8:
-        _dieToShoot = Instantiate(_d8Prefab, transform);
-        break;
-      case 12:
-        _dieToShoot = Instantiate(_d12Prefab, transform);
-        break;
-      case 20:
-        _dieToShoot = Instantiate(_d20Prefab, transform);
-        break;
-    }
-  }
-
-	public void Animate()
+	void Update()
 	{
-		_anim.SetTrigger("flick");
+
+	}
+
+	public void Throw()
+	{
+		int diceSides = WaveAuthority.PlayerRef.GetAmmo();
+		if (diceSides == 0)
+		{
+			// If there was no dice in bag, do nothing
+			return;
+		}
+
+		GameObject dieToShoot = null;
+
+		switch (diceSides)
+		{
+			case 4:
+				dieToShoot = Instantiate(DiceAuthority.Ref.GetDiceBySides(4), transform.position, transform.rotation, _projectileHolder.transform);
+				break;
+			case 6:
+				dieToShoot = Instantiate(DiceAuthority.Ref.GetDiceBySides(6), transform.position, transform.rotation, _projectileHolder.transform);
+				break;
+			case 8:
+				dieToShoot = Instantiate(DiceAuthority.Ref.GetDiceBySides(8), transform.position, transform.rotation, _projectileHolder.transform);
+				break;
+			case 12:
+				dieToShoot = Instantiate(DiceAuthority.Ref.GetDiceBySides(12), transform.position, transform.rotation, _projectileHolder.transform);
+				break;
+			case 20:
+				dieToShoot = Instantiate(DiceAuthority.Ref.GetDiceBySides(20), transform.position, transform.rotation, _projectileHolder.transform);
+				break;
+		}
+
+		if (dieToShoot)
+		{
+			DiceProjectile dieProj = dieToShoot.AddComponent<DiceProjectile>();
+			dieProj.InitializeProjectile(WaveAuthority.PlayerRef.GetProjectileSpeed(), WaveAuthority.PlayerRef.GetProjectileTTL());
+			dieProj.Shoot();
+		}
+
 	}
 
 }
--- Assets/_Scripts/Projectiles/DiceProjectile.cs
+++ Assets/_Scripts/Projectiles/DiceProjectile.cs
@@ -1,57 +1,77 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class DiceProjectile : MonoBehaviour
 {
-  [Header("Ammo Statics")]
-  [SerializeField] float speed;
-  [SerializeField] float lifeSpan;
-  bool alreadyHit;
+	[Header("Ammo Statics")]
+	[SerializeField] float speed;
+	[SerializeField] float lifeSpan;
+	bool alreadyHit;
 
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  void Start()
-  {
-    alreadyHit = false;
-  }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		alreadyHit = false;
+	}
 
-  // Update is called once per frame
-  void Update()
-  {
-    lifeSpan -= Time.deltaTime;
-    if (lifeSpan <= 0)
-    {
-      Destroy(gameObject);
-    }
-  }
+	// Update is called once per frame
+	void Update()
+	{
+		lifeSpan -= Time.deltaTime;
+		if (lifeSpan <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
 
-  public void Shoot()
-  {
-    Rigidbody rb = GetComponent<Rigidbody>();
-    Vector3 forceDirection = transform.forward;
-    Transform player = WaveAuthority.PlayerRef.transform;
+	public void InitializeProjectile(float speed, float lifeSpan)
+	{
+		this.speed = speed;
+		this.lifeSpan = lifeSpan;
+	}
 
-    RaycastHit hit;
-    if (Physics.Raycast(player.position + new Vector3(0f, .75f, 0f), transform.forward, out hit, 500f))
-    {
-      if (Vector3.Distance(hit.point, transform.position) > 0.5)
-      {
-        forceDirection = (hit.point - transform.position).normalized;
-      }
-    }
+	public void Shoot()
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+		Vector3 forceDirection = WaveAuthority.PlayerRef.RightTransform();
+		Transform player = WaveAuthority.PlayerRef.transform;
 
-    Vector3 force = forceDirection * speed;
-    transform.SetParent(null, true);
-    rb.AddForce(force, ForceMode.Impulse);
-  }
+		RaycastHit hit;
+		if (Physics.Raycast(player.position + new Vector3(0f, .75f, 0f), transform.forward, out hit, 500f))
+		{
+			if (Vector3.Distance(hit.point, transform.position) > 0.5)
+			{
+				forceDirection = (hit.point - transform.position).normalized;
+			}
+		}
 
-  void OnCollisionEnter (Collision other)
-  {
-    GameObject enemy = other.gameObject;
+		float pitch = -player.GetComponent<PlayerControl>().GetPitch();
 
-    if (enemy.GetComponent<Enemy>() && !alreadyHit)
-    {
-      alreadyHit = true;
-      enemy.GetComponent<Health>().TakeDamage(WaveAuthority.PlayerRef.GetProjectileDamage());
-      GetComponent<Rigidbody>().linearVelocity /= 4;
-    }
-  }
+		forceDirection.y = Mathf.Abs(pitch) / pitch * (1f + .1f * Mathf.Abs(pitch)) / 5f;
+		if (forceDirection.y < 0)
+		{
+			forceDirection.y += 0.2f;
+			forceDirection.x *= 1.6f;
+			forceDirection.z *= 1.6f;
+		}
+
+		Debug.Log(-player.GetComponent<PlayerControl>().GetPitch());
+
+		Vector3 force = forceDirection * speed;
+		transform.SetParent(null, true);
+		rb.AddForce(force, ForceMode.Impulse);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		GameObject enemy = other.gameObject;
+
+		if (enemy.GetComponent<Enemy>() && !alreadyHit)
+		{
+			alreadyHit = true;
+			enemy.GetComponent<Health>().TakeDamage(WaveAuthority.PlayerRef.GetProjectileDamage());
+			GetComponent<Rigidbody>().linearVelocity /= 4;
+		}
+	}
 }
