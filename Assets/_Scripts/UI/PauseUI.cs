fb69ad266f432e4e21bf946da56136b9c7f03244
garrowbr <garrowbr@msu.edu>
2026-02-21 13:42:39 -0600

    Start Pause Menu

---
+++ Assets/_Scripts/UI/PauseUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+
+public class PauseUI : MonoBehaviour
+{
+
+	[Header("Elements")]
+	[SerializeField] SettingsMenu _settingsMenu;
+	[SerializeField] ControlsMenu _controlsMenu;
+
+}
