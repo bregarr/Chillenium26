a98a31b447611ced3304318a74aa8e6fabb0357d
twilkes2026 <twilkes@tamu.edu>
2026-02-21 16:35:23 -0600

    Health Bar Add

--- Assets/_Scripts/UI/UIHealth.cs
+++ Assets/_Scripts/UI/UIHealth.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHealth : MonoBehaviour
 {
+  [Header("UI Components")]
+  [SerializeField] Image _bar;
+  Health health;
+
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
-      
+    
   }
 
   // Update is called once per frame
   void Update()
   {
-      
+    health = WaveAuthority.PlayerRef.GetComponent<Health>();
+    _bar.fillAmount = health.GetHealth() / health.GetMaxHealth();
   }
 }
