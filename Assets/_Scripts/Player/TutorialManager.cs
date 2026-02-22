ef6a4a5c12d03c9a5d40ac801f1324558c4f6b64
garrowbr <garrowbr@msu.edu>
2026-02-22 10:48:20 -0600

    Tutorialize

--- Assets/_Scripts/Player/TutorialManager.cs
+++ Assets/_Scripts/Player/TutorialManager.cs
@@ -1,47 +1,69 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
 
-    public static TutorialManager Ref { get; private set; }
+	public static TutorialManager Ref { get; private set; }
 
-    [Header("Elements")]
-    [SerializeField] GameObject _tutorialEnemy;
-    [SerializeField] GameObject _tutorialPrompt;
+	[Header("Elements")]
+	[SerializeField] GameObject _tutorialEnemy;
+	[SerializeField] GameObject _tutorialPrompt;
+	[SerializeField] Sprite[] _tutorialSprites;
 
-    bool _isInTutorial;
+	int _tutorialIndex;
 
-    void Start()
-    {
-        if (Ref)
-        {
-            Debug.LogWarning("Two tutorials in the scene");
-        }
-        Ref = this;
-    }
+	bool _startedSequence;
+	bool _isInTutorial;
 
-    void Update()
-    {
-        if (!_tutorialEnemy)
-        {
-            _isInTutorial = false;
-            if (_tutorialPrompt)
-            {
-                Destroy(_tutorialPrompt, 1.0f);
-                _tutorialPrompt.GetComponentInChildren<TMP_Text>().text = "Good work!";
-                WaveAuthority.Ref.StartFirstWave();
-            }
-        }
-        else
-        {
-            _isInTutorial = true;
-        }
-    }
+	void Start()
+	{
+		_startedSequence = false;
+		_tutorialIndex = 0;
+		if (Ref)
+		{
+			Debug.LogWarning("Two tutorials in the scene");
+		}
+		Ref = this;
+	}
 
-    public bool IsInTutorial()
-    {
-        return _isInTutorial;
-    }
+	void Update()
+	{
+		if (!_tutorialEnemy)
+		{
+			_isInTutorial = false;
+			if (_tutorialPrompt && !_startedSequence)
+			{
+				_startedSequence = true;
+				Invoke(nameof(IncrementTutorials), 0.25f);
+			}
+		}
+		else
+		{
+			_isInTutorial = true;
+		}
+	}
+
+	void IncrementTutorials()
+	{
+		_tutorialPrompt.GetComponentInChildren<TMP_Text>().text = "";
+		_tutorialPrompt.GetComponent<UnityEngine.UI.Image>().sprite = _tutorialSprites[_tutorialIndex];
+		_tutorialIndex++;
+		if (_tutorialIndex >= _tutorialSprites.Length)
+		{
+			Destroy(_tutorialPrompt, 3.5f);
+			WaveAuthority.Ref.StartFirstWave();
+		}
+		else
+		{
+			Invoke(nameof(IncrementTutorials), 3.5f);
+		}
+	}
+
+	public bool IsInTutorial()
+	{
+		return _isInTutorial;
+	}
 
 }
